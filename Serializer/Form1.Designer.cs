b955fd5eacd4a8ade5210a32691051e758971b4d
belu82 <44766864+belu82@users.noreply.github.com>
2019-12-02 10:44:38 +0100

    Add project files.

---
+++ Serializer/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Serializer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.lastButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(69, 35);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(274, 22);
+            this.textName.TabIndex = 0;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(69, 98);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(274, 22);
+            this.textAddress.TabIndex = 1;
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(69, 154);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(274, 22);
+            this.textPhone.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(69, 12);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 17);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(72, 78);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(60, 17);
+            this.addressLabel.TabIndex = 4;
+            this.addressLabel.Text = "Address";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(72, 131);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(49, 17);
+            this.phoneLabel.TabIndex = 5;
+            this.phoneLabel.Text = "Phone";
+            // 
+            // prevButton
+            // 
+            this.prevButton.Location = new System.Drawing.Point(69, 216);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(75, 23);
+            this.prevButton.TabIndex = 6;
+            this.prevButton.Text = "Previous";
+            this.prevButton.UseVisualStyleBackColor = true;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(169, 216);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 7;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            // 
+            // lastButton
+            // 
+            this.lastButton.Location = new System.Drawing.Point(268, 216);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new System.Drawing.Size(75, 23);
+            this.lastButton.TabIndex = 8;
+            this.lastButton.Text = "Last";
+            this.lastButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 338);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.textName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button lastButton;
+    }
+}
+
